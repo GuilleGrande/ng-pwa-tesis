ded2b71c009726de43bb12a90f777d5c43219913
GuilleGrande <gillegrande@gmail.com>
2018-08-26 18:20:19 +0200

    Fixes sign in config.

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -5,10 +5,12 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { errorHandler } from '@angular/platform-browser/src/browser';
 import { error } from 'util';
 import { Injectable } from '@angular/core';
+import { Md5 } from 'ts-md5/dist/md5';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
+
 
 interface User {
   uid: string,
@@ -26,48 +28,49 @@ export class AuthService {
 
   user: Observable<User>
 
-  constructor( private afAuth: AngularFireAuth, 
-               private db: AngularFirestore, 
-               private router: Router) { 
-      
-      this.user = this.afAuth.authState.switchMap(user => {
-        if (user) {
-          return this.db.doc<User>(`users/${user.uid}`).valueChanges();
-        }
-        else {
-          return of(null);
-        }
-      })
+  constructor(private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router) {
+
+    this.user = this.afAuth.authState.switchMap(user => {
+      if (user) {
+        return this.db.doc<User>(`users/${user.uid}`).valueChanges();
+      }
+      else {
+        return of(null);
+      }
+    })
   }
 
   emailSignIn(email: string, password: string) {
     return this.afAuth.auth.signInWithEmailAndPassword(email, password)
-            .then(() => console.log("You have succesfully signed in"))
-            .catch(error => console.log(error.message))
+      .then(() => console.log("You have succesfully signed in"))
+      .catch(error => console.log(error.message))
   }
 
   emailSignUp(email: string, password: string) {
     console.log('From emailSignUp() Email: ' + email);
     return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
-            .then((user) => { this.updateUserData(user)})
-            .then(() => console.log("Welcome, your account has been created"))
-            .catch(error => console.log('Error code : ' + error.code + '| Error message: ' + error.message));
+      .then((userRef) => { this.updateUserData(userRef) })
+      .then(() => console.log("Welcome, your account has been created"))
+      .catch(error => console.log('Error code : ' + error.code +
+        '| Error message: ' + error.message));
   }
 
   signOut() {
     return this.afAuth.auth.signOut()
-            .then(() => { this.router.navigate(['/'])})
+      .then(() => { this.router.navigate(['/']) })
   }
 
-  private updateUserData(user) {
-    const userRef: AngularFirestoreDocument<User> = this.db.doc(`users/${user.uid}`);
+  private updateUserData(users) {
+    const userRef: AngularFirestoreDocument<User> = this.db.doc(`users/${users.user.uid}`);
     const data: User = {
-      uid: user.uid,
-      email: user.email || null,
+      uid: users.user.uid,
+      email: users.user.email || null,
       //firstName: user.firstName,
       //lastName: user.lastName,
-      displayName: user.displayName,
-      photoUrl: user.photoUrl
+      displayName: users.user.displayName,
+      photoUrl: "https://www.gravatar.com/avatar/" + Md5.hashStr(users.user.uid) + "?d=identicon"
     }
     return userRef.set(data, { merge: true });
   }
